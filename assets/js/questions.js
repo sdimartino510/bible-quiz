69fd74796dffc22286089684222b97faee57823e
Sergio Di Martino <sergio@webdevserg.io>
2020-05-06 14:56:54 -0700

    Begins adding questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,35 +1,34 @@
 // list of all questions, choices, and answers
-var questions = [
+
+const questions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
-  },
-  {
-    title: "Arrays in JavaScript can be used to store ____.",
+    title: "How many books are there in the Old Testament?",
     choices: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above"
+      "27",
+      "66",
+      "39",
+      "42"
     ],
-    answer: "all of the above"
+    answer: "39"
   },
   {
-    title:
-      "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: "quotes"
+    title: "How many books are there in the New Testament?",
+    choices: [
+      "27",
+      "66",
+      "39",
+      "42"
+    ],
+    answer: "27"
   },
   {
-    title:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-    answer: "console.log"
+    title: "In what city was Jesus born?",
+    choices: [
+      "Nazareth",
+      "Galilee",
+      "Jerusalem",
+      "Bethlehem"
+    ],
+    answer: "Bethlehem"
   }
-];
+]
